4cad825cebad7690a8d055a712ad13d1ead73a12
verycloud <wu.shuaiwei@163.com>
2020-01-01 00:16:40 +0800

    交换机备份

---
+++ backup_config/change.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+mv /home/Script/Backtest/S6720/10.200.1.2 /home/Script/Backtest/S6720/NJ_JNL_H6720_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/S6720/10.200.12.1 /home/Script/Backtest/S6720/JJ-H6720-ISP_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/S6720/10.200.12.2 /home/Script/Backtest/S6720/TaZ_4-3_01-H6720_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/S6720/10.200.12.3 /home/Script/Backtest/S6720/TaZ_4-3_02-H6720_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/S6720/10.200.2.1 /home/Script/Backtest/S6720/WX-JD-H6720_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/S6720/10.200.2.2 /home/Script/Backtest/S6720/JY-CO-A03-H6720_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/S6720/10.200.2.3 /home/Script/Backtest/S6720/WX-LX-H6720_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/S6720/10.200.2.4 /home/Script/Backtest/S6720/JY-GD-E07-H6720_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/S6720/10.200.2.5 /home/Script/Backtest/S6720/WX-PBS-H6720_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/S6720/10.200.4.1 /home/Script/Backtest/S6720/CZ-01-G09-H6720-A_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/S6720/10.200.4.2 /home/Script/Backtest/S6720/CZ-LX-H6720_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/S6720/10.200.6.1 /home/Script/Backtest/S6720/NT-01-CTC-B1-H6720_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/S6720/10.200.6.2 /home/Script/Backtest/S6720/NT-YMG-R092-H6720_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/S6720/10.201.1.1 /home/Script/Backtest/S6720/SH-HC-A02-H6720_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/S6720/10.201.1.11 /home/Script/Backtest/S6720/SH-LZZH-A01-H6720_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/S6720/10.201.1.13 /home/Script/Backtest/S6720/SH-JG-S6720_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/S6720/10.201.1.18 /home/Script/Backtest/S6720/SH-MLY-H6720_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/S6720/10.201.1.3 /home/Script/Backtest/S6720/SH-LC-A01-H6720_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/S6720/10.201.1.4 /home/Script/Backtest/S6720/SH-PBS-H6720_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/S6720/10.201.1.5 /home/Script/Backtest/S6720/SH-WJL-H6720_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/S6720/10.201.1.7 /home/Script/Backtest/S6720/SH-04-H6720-A_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/S6720/10.202.1.19 /home/Script/Backtest/S6720/HZ-JY-S6720_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/S6720/10.202.1.4 /home/Script/Backtest/S6720/HZ-CQ-H6720-A_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/S6720/10.202.1.5 /home/Script/Backtest/S6720/HZ-CQ-H6720-B_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/S6720/10.202.1.9 /home/Script/Backtest/S6720/HZ-YD-H6720_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/S6720/10.202.7.2 /home/Script/Backtest/S6720/JH-02-A02-S6720_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/S6720/10.204.4.1 /home/Script/Backtest/S6720/ZaoZ-LN-S6720_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/S6720/10.204.4.2 /home/Script/Backtest/S6720/ZaoZ-GMHY-S6720_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/S6720/10.206.4.1 /home/Script/Backtest/S6720/ST-SR-PBS-H6720-A_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/S6720/10.1.98.18 /home/Script/Backtest/S6720/SH_BL_Core_H5720_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/S6720/10.1.99.15 /home/Script/Backtest/S6720/SH-LC-H5720_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/S6720/10.1.99.17 /home/Script/Backtest/S6720/SH-04-H5720-A_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/S6720/10.2.99.47 /home/Script/Backtest/S6720/HZ-TDGC-H5720_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/S6720/10.200.2.8 /home/Script/Backtest/S6720/WX-JYZ-H5720_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/S6720/10.200.2.9 /home/Script/Backtest/S6720/WX-JD-H5720_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/S6720/10.201.1.10 /home/Script/Backtest/S6720/SH-HC-A2-S5720_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/S6720/10.201.1.12 /home/Script/Backtest/S6720/SH-BS-H5720_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/S6720/10.201.1.20 /home/Script/Backtest/S6720/SH-HY-A01-H5720_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/S6720/10.201.1.23 /home/Script/Backtest/S6720/SH-JRJ-A01-S5720_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/S6720/10.202.1.14 /home/Script/Backtest/S6720/HZ-Ty-5720_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/S6720/10.202.1.16 /home/Script/Backtest/S6720/HZ-YouPai-S5720_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/S6720/10.1.98.19 /home/Script/Backtest/S6720/SH-BeiAi-S5700_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/S6720/10.1.98.21 /home/Script/Backtest/S6720/SH-LJZ-S5700_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/S6720/10.1.99.12 /home/Script/Backtest/S6720/SH-LHL-H5700_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/S6720/10.205.5.1 /home/Script/Backtest/S6720/MaAnShan-B26-H6720_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/S6720/10.200.1.5 /home/Script/Backtest/S6720/NJ-ChengYi6720_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/S6720/10.200.1.6 /home/Script/Backtest/S6720/NJ-YouChi-6720_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/S6720/10.200.1.7 /home/Script/Backtest/S6720/NJ_GWBN-6720_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/S6720/10.200.1.8 /home/Script/Backtest/S6720/NJ-ZX-6720_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/S6720/10.205.5.2 /home/Script/Backtest/S6720/MaAnShan-XiangYun-6720-DX_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/S6720/10.202.1.20 /home/Script/Backtest/S6720/HZ-YD-2-H6720_`date "+%Y-%m-%d"`
+#
+#
+mv /home/Script/Backtest/CE6870/10.200.1.3 /home/Script/Backtest/CE6870/NJ-03-JQ-CE6870_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/CE6870/10.200.10.1 /home/Script/Backtest/CE6870/YZ-CE6870_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/CE6870/10.200.2.6 /home/Script/Backtest/CE6870/WX-CMCC-L14-CE6870_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/CE6870/10.200.4.3 /home/Script/Backtest/CE6870/CZ-01-G09-CE6870-A_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/CE6870/10.200.5.1 /home/Script/Backtest/CE6870/SZ-XHY-G03-CE6870_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/CE6870/10.200.6.3 /home/Script/Backtest/CE6870/NT-CMCC-K1_11-CE6870_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/CE6870/10.200.6.4 /home/Script/Backtest/CE6870/NT-QT-A02-CE6870_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/CE6870/10.200.7.1 /home/Script/Backtest/CE6870/LYG-01-B00-CE6870_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/CE6870/10.200.7.2 /home/Script/Backtest/CE6870/LYG-02-A01-CE6870_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/CE6870/10.200.8.1 /home/Script/Backtest/CE6870/HA-CUC-B07-CE6870_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/CE6870/10.201.1.2 /home/Script/Backtest/CE6870/SH-B3-LY-CE6870_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/CE6870/10.201.1.24 /home/Script/Backtest/CE6870/SH-A01-PBS-CE6870_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/CE6870/10.201.1.8 /home/Script/Backtest/CE6870/SH-LX-0203-K59-CE6870_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/CE6870/10.202.1.2 /home/Script/Backtest/CE6870/HaZ-01-CE6870_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/CE6870/10.202.4.1 /home/Script/Backtest/CE6870/JX-01-5-25-CE6870_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/CE6870/10.202.5.1 /home/Script/Backtest/CE6870/HuZhou-01-CE6870_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/CE6870/10.202.7.1 /home/Script/Backtest/CE6870/JH-01-A710-CE6870_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/CE6870/10.200.11.1 /home/Script/Backtest/CE6870/ZJ-CRC-A01-CE6800_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/CE6870/10.205.5.3 /home/Script/Backtest/CE6870/MaAnShan_YuShanXiCun-6855_`date "+%Y-%m-%d"`
+mv /home/Script/Backtest/CE6870/10.200.1.4 /home/Script/Backtest/CE6870/NJ-JQ-DX685_`date "+%Y-%m-%d"`
+#
+#
+cp -f /home/Script/Backtest/CE6870/* /home/Script/Backtest/all/
+cp -f /home/Script/Backtest/S6720/* /home/Script/Backtest/all/
+mv -f /home/Script/Backtest/all /home/backup/all_`date "+%Y-%m-%d"`
+mv -f /home/Script/Backtest/CE6870 /home/backup/CE6870_`date "+%Y-%m-%d"`
+mv -f /home/Script/Backtest/S6720 /home/backup/S6720_`date "+%Y-%m-%d"`
+mkdir /home/Script/Backtest/S6720 && mkdir /home/Script/Backtest/CE6870 && mkdir /home/Script/Backtest/all
